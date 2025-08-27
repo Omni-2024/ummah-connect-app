cd7c8ce9a15e3aeabc659e6562b8fea557c83095
saliha-ziyard <salihaziyard@gmail.com>
2025-08-28 04:43:40 +0530

    setup Next.js project

---
+++ user-panel/src/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Welcome to Next.js 🚀</h1>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            Get started by editing <code>src/app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>Built with ❤️ using Next.js</p>
+      </footer>
+    </div>
+  );
+}
