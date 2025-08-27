9bc49d5eb7b4e22897e795c4c858500472b68006
Sudesh Tharoosh <zoimbu73@gmail.com>
2025-08-27 14:57:31 +0530

    deleted app-service

--- api-service/auth-service/src/main/java/com/app/ummahConnect/authservice/model/Role.java
+++
@@ -1,8 +0,0 @@
-package com.app.ummahConnect.authservice.model;
-
-public enum Role {
-    SUPER_ADMIN,
-    ADMIN,
-    PROVIDER,
-    CONSUMER
-}
--- api-service/course-service/src/main/java/com/course_service/service/exception/CourseNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.course_service.service.exception;
-
-
-public class CourseNotFoundException extends RuntimeException {
-    public CourseNotFoundException(String message) {
-        super(message);
-    }
-}
-
