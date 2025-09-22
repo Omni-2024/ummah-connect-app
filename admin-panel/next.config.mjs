b3554c78e9279bbbff2c14fe7917b68de80c61b6
aathiq <aathiq.20210466@iit.ac.lk>
2025-09-23 04:29:10 +0530

    next config updated

--- admin-panel/next.config.mjs
+++ admin-panel/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: [
       "pub-08208f7576844a018495f0edfac47490.r2.dev",
+      "lh3.googleusercontent.com"
     ],
   },
 };
--- user-panel/next.config.ts
+++ user-panel/next.config.ts
@@ -4,9 +4,8 @@ const nextConfig: NextConfig = {
     images: {
         // ✅ hostnames only (no protocol, no path)
         domains: [
-            "pub-08208f7576844a018495f0edfac47490.r2.dev", // replace with your real public host
-            // "cdn.yourdomain.com",                        // if you mapped a custom domain
-            // "5ce46b8075c72e337577b3e116da44a5.r2.cloudflarestorage.com", // only if you truly serve from API endpoint (not recommended)
+            "pub-08208f7576844a018495f0edfac47490.r2.dev",
+            "lh3.googleusercontent.com",
         ],
 
         // OR you can use remotePatterns instead of domains:
