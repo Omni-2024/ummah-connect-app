e65c6bc6e59e22d27bd2fe440197089f82b3a240
saliha-ziyard <salihaziyard@gmail.com>
2025-10-19 02:19:49 +0530

    margin top fixed

--- user-panel/src/features/explore/component/QuoteRequestModal.tsx
+++ user-panel/src/features/explore/component/QuoteRequestModal.tsx
@@ -91,7 +91,7 @@ export default function QuoteRequestModal({
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
-      <div className="bg-white rounded-lg shadow-xl w-full max-w-lg max-h-[70vh] lg:max-h-[90vh]  overflow-y-auto">
+      <div className="bg-white rounded-lg shadow-xl w-full max-w-lg max-h-[70vh] lg:max-h-[80vh]  overflow-y-auto mt-14">
         {/* Modal Header */}
         <div className="flex items-center justify-between p-4 sm:p-6 border-b border-gray-200 sticky top-0 bg-white rounded-t-lg">
           <h2 className="text-lg sm:text-xl font-semibold text-gray-900">Request A Quote</h2>
