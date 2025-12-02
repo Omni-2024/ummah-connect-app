fd984599b59811a06204cb4ec8e383499041967d
aathiq <aathiq.20210466@iit.ac.lk>
2025-12-02 21:33:44 +0530

    App state added to admin panel

---
+++ admin-panel/features/app/context/useAppState.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { useSnapshot } from 'valtio';
+import {
+    appState, setOnboardingSuccess,
+} from './AppState';
+
+export const useAppState = () => {
+    const snap = useSnapshot(appState);
+    return {
+        onboardingSuccess: snap.onboardingSuccess,
+        setOnboardingSuccess:setOnboardingSuccess,
+    };
+};
